6cd01075f0bcea7545ec63caefb6cf5ebf19afc1
stevevillardi <stevevillardi@gmail.com>
2020-03-06 18:29:42 -0500

    remove example code

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-    Book: require("./book"),
     User: require("./user"),
     Environment: require("./environment"),
     Mailbox: require("./mailbox")
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
 const userRoutes = require("./user");
 const mailboxRoutes = require("./mailbox");
 const environmentRoutes = require("./environment");
 
-// Book routes
-router.use("/books", bookRoutes);
 router.use("/users", userRoutes);
 router.use("/mailboxes", mailboxRoutes);
 router.use("/environments", environmentRoutes);
